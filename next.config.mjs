176331192aca97773e27f1612a50087a9f97a2ad
howens157 <haydenowens52@gmail.com>
2024-08-05 22:11:56 -0400

    fix build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'standalone',
+  };
+  
+  export default nextConfig;
